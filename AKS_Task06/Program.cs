7dc8a387ffd670bf32740270211d049435240c69
kirillaleynikov <kiryusha-aleynikov@mail.ru>
2023-04-20 11:58:49 +0300

    .

--- AKS_Task06/Program.cs
+++ AKS_Task06/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace AKS_Task06
 {
@@ -14,65 +10,70 @@ namespace AKS_Task06
             string[] posts = new string[0];
             string[] fullNames = new string[0];
             bool isExitProgram = true;
-            while (isExitProgram)
-            {
-                Console.Clear();
-                Console.WriteLine($"1 - Добавить досье\n" + 
-                    $"2 - Вывод всех досье на экран с их ID в программе\n" +
-                    $"3 - Поиск досье по ФИО\n" +
-                    $"4 - Удалить досье по ID\n" + 
-                    $"5 - Выход из программы\n");
-                switch (Console.ReadLine())
+                while (isExitProgram)
                 {
-                    case "1":
-                        Console.Clear();
-                        AddDossier(ref fullNames, ref posts);
-                        break;
-                    case "2":
-                        Console.Clear();
-                        ShowAllDossiers(posts, fullNames);
-                        break;   
-                    case "3":
-                        Console.Clear();
-                        SearchDossier(fullNames, posts);
-                        break;
-                    case "4":
-                        Console.Clear();
-                        DeleteDossier(ref fullNames, ref posts);
-                        break;
-                    case "5":
-                        isExitProgram = false;
-                        break;
+                    Console.Clear();
+                    Console.WriteLine($"1 - Добавить досье\n" +
+                        $"2 - Вывод всех досье\n" +
+                        $"3 - Поиск досье по ФИО\n" +
+                        $"4 - Удалить досье по ID\n" +
+                        $"5 - Выход из программы\n");
+                    switch (Console.ReadLine()) //выбор пунктов меню
+                    {
+                        case "1":
+                            Console.Clear();
+                            AddDossier(ref fullNames, ref posts);
+                            break;
+                        case "2":
+                            Console.Clear();
+                            ShowAllDossiers(posts, fullNames);
+                            break;
+                        case "3":
+                            Console.Clear();
+                            SearchDossier(fullNames, posts);
+                            break;
+                        case "4":
+                            Console.Clear();
+                            DeleteDossier(ref fullNames, ref posts);
+                            break;
+                        case "5":
+                            isExitProgram = false;
+                            break;
+                    }
+                    Console.WriteLine(new string('-', 100));
+                    Console.Write("Нажмите любую клавишу для продолжения работы");
+                    Console.ReadKey();
                 }
-                Console.WriteLine(new string('-', 100));
-                Console.Write("Нажмите любую клавишу для продолжения работы");
-                Console.ReadKey();
-            }
         }
 
-        private static void AddDossier(ref string[] names, ref string[] posts)
+        private static void AddDossier(ref string[] names, ref string[] posts) //добавление досье
         {
+            Console.WriteLine("exit - Перейти в главное меню\n");
             Console.Write("Введите ФИО: ");
             string name = Console.ReadLine();
+            if (name == "exit")
+            {
+                return;
+            }
             Console.Write("Введите должность: ");
             string post = Console.ReadLine();
-            names = IncreasArray(names, name);
-            posts = IncreasArray(posts, post);
+            names = IncreasArray(names, name); //обращение к методу добавления ФИО и должности в массив, передача массива с ФИО и нового элемента
+            posts = IncreasArray(posts, post); //обращение к методу добавления ФИО и должности в массив, передача массива с должностями и новой должности
         }
 
-        private static string[] IncreasArray(string[] array, string text)
+        private static string[] IncreasArray(string[] array, string text) //добавление ФИО и должности в массив
         {
-            string[] temporaryArray = new string[array.Length + 1];
+            string[] bufArray = new string[array.Length + 1]; //временный массив
             for (int i = 0; i < array.Length; i++)
             {
-                temporaryArray[i] = array[i];
+                bufArray[i] = array[i]; //добавление ногового элемента в массив
             }
-            temporaryArray[temporaryArray.Length - 1] = text;
-            array = temporaryArray;
+            bufArray[bufArray.Length - 1] = text;
+            array = bufArray;
             return array;
         }
 
-        private static void ShowAllDossiers(string[] posts, string[] fullNames)
+        private static void ShowAllDossiers(string[] posts, string[] fullNames) //отображение всех досье
         {
             int id = 1;
             for (int i = 0; i < posts.Length; i++)
@@ -82,15 +83,25 @@ namespace AKS_Task06
             }
         }
 
-        private static void DeleteDossier(ref string[] fullNames, ref string[] posts)
+        private static void DeleteDossier(ref string[] fullNames, ref string[] posts) //удаление досье
         {
+            int id2 = 1;
+            for (int i = 0; i < posts.Length; i++) //вывод всех досье
+            {
+                Console.WriteLine($"ID [{id2}] | ФИО : {fullNames[i]} | должность : {posts[i]}");
+                id2++;
+            }
             Console.Write("Введите номер досье: ");
             int number = int.Parse(Console.ReadLine());
+            if (Convert.ToString(number) == "1")
+            {
+                return;
+            }
             if (number > 0 && number <= fullNames.Length)
             {
                 int id = number - 1;
-                fullNames = DecreaseArray(fullNames, id);
-                posts = DecreaseArray(posts, id);
+                fullNames = DecreaseArray(fullNames, id); //обращение к методу удаления ФИО и должности из массива, передача массива с ФИО и id, по которому произойдёт удаление
+                posts = DecreaseArray(posts, id); //обращение к методу удаления ФИО и должности из массива, передача массива с должностью и id, по которому произойдёт удаление
                 Console.WriteLine($"Досье с ID [{number}] удалено");
             }
             else
@@ -99,39 +110,43 @@ namespace AKS_Task06
             }
         }
 
-        private static string[] DecreaseArray(string[] array, int id)
+        private static string[] DecreaseArray(string[] array, int id) //удаление ФИО и должности из массива
         {
-            string[] temporaryArray = new string[array.Length - 1];
+            string[] bufArray = new string[array.Length - 1]; //временный массив
             for (int i = 0; i < id; i++)
             {
-                temporaryArray[i] = array[i];
+                bufArray[i] = array[i];
             }
             for (int i = id; i < array.Length - 1; i++)
             {
-                temporaryArray[i] = array[i + 1];
+                bufArray[i] = array[i + 1];
             }
-            array = temporaryArray;
+            array = bufArray;
             return array;
         }
 
-        private static void SearchDossier(string[] fullNames, string[] posts)
+        private static void SearchDossier(string[] fullNames, string[] posts) //поиск досье по фамилии
         {
+            Console.WriteLine("exit - Перейти в главное меню\n");
             Console.Write("Введите фамилию для поиска досье: ");
-            string surname = Console.ReadLine();
-            bool isSuccessfulSearch = false;
+            string lastName = Console.ReadLine();
+            if (lastName == "exit")
+            {
+                return;
+            }
+            bool search = false;
             for (int i = 0; i < fullNames.Length; i++)
             {
-                string[] split = fullNames[i].Split(' ');
-
-                if (split[0].ToLower() == surname.ToLower())
+                string[] surname = fullNames[i].Split(' '); //взятие фамилии из ФИО
+                if (surname[0].ToLower() == lastName.ToLower())
                 {
                     Console.WriteLine($"ID [{i + 1}] | ФИО : {fullNames[i]} | должность : {posts[i]}");
-                    isSuccessfulSearch = true;
+                    search = true;
                 }
             }
-            if (isSuccessfulSearch == false)
+            if (search == false)
             {
-                Console.WriteLine($"Досье с фамилией '{surname}' не найдено");
+                Console.WriteLine($"Досье с фамилией '{lastName}' не существует");
             }
         }
     }
--- AKS_Task07/Program.cs
+++ AKS_Task07/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AKS_Task07
 {
@@ -10,6 +6,29 @@ namespace AKS_Task07
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            Console.Write("Введите количество элементов массива: ");
+            int[] mas1 = new int[Convert.ToInt32(Console.ReadLine())];
+            for (int i=0; i<mas1.Length; i++)
+            {
+                mas1[i] = rnd.Next(-50,50);
+            }
+            Console.WriteLine("Введённый массив:");
+            for (int i = 0; i < mas1.Length; i++)
+            {
+                Console.Write(mas1[i]+" ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Перемешанный массив:");
+            for (int i = mas1.Length - 1; i>=0; i--)
+            {
+                int rnd2 = rnd.Next(i);
+                int shuffledElement = mas1[rnd2];
+                mas1[rnd2] = mas1[i];
+                mas1[i] = shuffledElement;
+                Console.Write(mas1[i] + " ");
+            }
+            Console.ReadKey();
         }
     }
 }
